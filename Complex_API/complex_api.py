dba42fc69bbdc9abc402370983a9bcaec73f28a9
JagTheFriend <jagthefriend12@gmail.com>
2021-03-27 14:45:05 +0200

    Updated version

--- Complex_API/complex_api.py
+++ Complex_API/complex_api.py
@@ -3,7 +3,7 @@ import requests
 API_URL = "https://API.jagthefriend.repl.co"
 GITHUB_REPO = "https://github.com/JagTheFriend/Complex-API"
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 __all__ = [
     "compile", "reddit", "lyrics",
     "ascii", "temp", "length",
